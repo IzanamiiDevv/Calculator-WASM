823cb54e1c64717eab76c97e21b0b0b41bf1d266
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-28 21:39:13 +0800

    instantiate wasm

--- app.js
+++ app.js
@@ -9,6 +9,36 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,'public');
 
+let wasmMemory = new WebAssembly.Memory({ initial: 256, maximum: 256 });
+let wasmTable = new WebAssembly.Table({
+    initial: 1,
+    maximum: 1,
+    element: 'anyfunc'
+});
+
+
+let asmLibraryArg = {
+    __handle_over_flow: () => {},
+    emscripten_resize_heap: () => {},
+    __lock: () => {},
+    __unlock: () => {},
+    memory: wasmMemory,
+    table: wasmTable
+};
+
+
+let info = {
+    env: asmLibraryArg,
+    wasi_snapshot_preview1: asmLibraryArg
+};
+
+
+async function instantiate() {
+    let response = await fetch('operator.wasm');
+    let bytes = await response.arrayBuffer();
+    let wasmObj = await WebAssembly.instantiate(bytes, info);
+    return new Promise(res => res(wasmObj.instance.exports))
+}
 
 //Middle Ware
 app.use(cors({
