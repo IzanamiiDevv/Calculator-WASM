2620f262ae6ae489dcb5b7caf3cb5110808bb85a
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-28 22:00:16 +0800

    wasm

--- Client/wasm/operator.cpp
+++ Client/wasm/operator.cpp
@@ -8,4 +8,19 @@ extern "C" {
     int add(int x,int y){
         return x + y;
     }
+
+    EMSCRIPTEN_KEEPALIVE
+    int subtract(int x,int y){
+        return x - y;
+    }
+
+    EMSCRIPTEN_KEEPALIVE
+    int multiply(int x,int y){
+        return x * y;
+    }
+
+    EMSCRIPTEN_KEEPALIVE
+    int devide(int x,int y){
+        return x / y;
+    }
 }
