f2e1c4360f57a6529cce177a855ba4dc79c48fb1
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-28 21:24:29 +0800

    express

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+//Importing Modules
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
+
+//Initalization
 const app = express();
 const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,'public');
 
+
+//Middle Ware
 app.use(cors({
     origin:"*"
 }));
 
+
+//Server Root
 app.get('/',(req,res)=>{
     res.sendFile(path.join(publicPath,'index.html'));
 });
 
+
+//Lunch the Server
 app.listen(PORT,()=>{
-    console.log(`server was running at: http://localhost:${PORT}`)
+    console.log(`server was running at: http://localhost:${PORT}`);
 });
