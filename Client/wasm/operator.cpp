e653fb9dad8c086b8280f7638534644c14cce781
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-28 23:00:18 +0800

    c++

--- Client/wasm/operator.cpp
+++ Client/wasm/operator.cpp
@@ -1,25 +1,25 @@
-#include<emscripten.h>
+//#include<emscripten.h>
 #include<iostream>
 
 using namespace std;
 
 extern "C" {
-    EMSCRIPTEN_KEEPALIVE
+    //EMSCRIPTEN_KEEPALIVE
     int add(int x,int y){
         return x + y;
     }
 
-    EMSCRIPTEN_KEEPALIVE
+    //EMSCRIPTEN_KEEPALIVE
     int subtract(int x,int y){
         return x - y;
     }
 
-    EMSCRIPTEN_KEEPALIVE
+    //EMSCRIPTEN_KEEPALIVE
     int multiply(int x,int y){
         return x * y;
     }
 
-    EMSCRIPTEN_KEEPALIVE
+    //EMSCRIPTEN_KEEPALIVE
     int devide(int x,int y){
         return x / y;
     }
